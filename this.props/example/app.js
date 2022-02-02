9cb077605bbe3085168c0f19d14c5f95683495cd
Shawn Mason <Shawnm@hallmarkcircuits.com>
2022-02-01 16:10:37 -0800

    Added React this.props

---
+++ this.props/example/app.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class PropsDisplayer extends React.Component {
+  render() {
+  	const stringProps = JSON.stringify(this.props);
+
+    return (
+      <div>
+        <h1>CHECK OUT MY PROPS OBJECT</h1>
+        <h2>{stringProps}</h2>
+      </div>
+    );
+  }
+}
+
+// ReactDOM.render goes here:
+ReactDOM.render(<PropsDisplayer myProp="Hello"/>, document.getElementById('app'))
+
+// another example of this:
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Greeting extends React.Component {
+  render() {
+    return <h1>Hi there, {this.props.firstName}!</h1>;
+  }
+}
+
+ReactDOM.render(
+  <Greeting firstName='Groberta' />, 
+  document.getElementById('app')
+);
