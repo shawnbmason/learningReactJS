b824d503631ba07b59510832a23eed59fe00512b
Shawn Mason <Shawnm@hallmarkcircuits.com>
2022-02-07 08:24:59 -0800

    this.state

---
+++ this.state/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	// constructor method begins here:
+
+	
+  render() {
+    return (
+      <h1>
+        Wow this entire app is just an h1.
+      </h1>
+    );
+  }
+}
