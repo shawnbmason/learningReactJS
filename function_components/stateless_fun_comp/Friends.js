b4f2cb9470f985646f0cc5b907fa13976fe7d637
Shawn Mason <Shawnm@hallmarkcircuits.com>
2022-02-09 16:11:55 -0800

    Function Components

---
+++ function_components/stateless_fun_comp/Friends.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export class Friend extends React.Component {
+  render() {
+    return <img src="https://content.codecademy.com/courses/React/react_photo-octopus.jpg" />;
+  }
+};
+
+ReactDOM.render(
+	<Friend />,
+	document.getElementById('app')
+);
