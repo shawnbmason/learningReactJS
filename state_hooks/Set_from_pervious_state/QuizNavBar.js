d297b4614b71b4342c8acfba77ca61fb736601d0
Shawn Mason <Shawnm@hallmarkcircuits.com>
2022-02-14 16:23:29 -0800

    almost there

--- state_hooks/Set_from_pervious_state/QuizNavBar.js
+++ state_hooks/Set_from_pervious_state/QuizNavBar.js
@@ -4,8 +4,12 @@ export default function QuizNavBar({ questions }) {
   const [questionIndex, setQuestionIndex] = useState(0);
 
   // define event handlers 
+  const goBack = () => setQuestionIndex((prevQuestionIndex) => prevQuestionIndex - 1);
 
-  // determine if on the first question or not 
+  const goToNext = () => setQuestionIndex((setQuestionIndex) => setQuestionIndex + 1);
+
+  // determine if on the first question or not
+  const onFirstQuestion = questionIndex === 0;
 
   const onLastQuestion = questionIndex === questions.length - 1;
 
@@ -13,10 +17,10 @@ export default function QuizNavBar({ questions }) {
     <nav>
       <span>Question #{questionIndex + 1}</span>
       <div>
-        <button>
+        <button onClick={goBack} disabled={onFirstQuestion}>
           Go Back
         </button>
-        <button disabled={onLastQuestion}>
+        <button onClick={goToNext} disabled={onLastQuestion}>
           Next Question
         </button>
       </div>
