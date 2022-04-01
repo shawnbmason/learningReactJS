1edf3f35252a9f5d4f6d362bdebb050cc84bd4d8
Shawn Mason <Shawnm@hallmarkcircuits.com>
2022-04-01 15:34:57 -0700

    JavaScript actions and React project

---
+++ JavaScript/.forEach()/main.js
@@ -0,0 +1,9 @@
+const fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+
+// Iterate over fruits below
+
+function printFruit(element){
+  console.log('I want to eat a ' + element);
+}
+
+fruits.forEach(printFruit);
