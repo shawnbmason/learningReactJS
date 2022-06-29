9bc7e57a2c19a7191a8dfe60e1c802e2ff1aef8b
Shawn Mason <Shawnm@hallmarkcircuits.com>
2022-06-29 16:00:35 -0700

    Combinding Componets

---
+++ ReactNative/combindingComponets/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => (
+  <View style={{ flex: 1, justifyContent: 'center' }}>
+    <Box color='red' />
+    <Box color='green' />
+    <Box color='blue' />
+  </View>
+);
+
+export default App;
+
+export const Box = (props) => (
+  // Move a box `View` component here
+   <View style={{ width: 100, height: 100, backgroundColor: props.color }} />
+);
